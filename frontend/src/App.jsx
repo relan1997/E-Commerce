1e996ccd388e52a53ffb0104a2ed6a539e5a566b
relan1997 <harshal.relan.201116@gmail.com>
2024-06-07 02:22:49 +0530

    Added an Individual Product Page with some Modifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Navigation from './customer/components/Navigation/Navigation'
 import HomePage from './customer/pages/HomePage/HomePage'
 import Footer from './customer/components/Footer/Footer'
 import Product from './customer/components/Product/Product'
+import ProductDetails from './customer/components/ProductDetails/ProductDetails'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,8 @@ function App() {
     <Navigation/>
     </div>
     {/* <HomePage/> */}
-    <Product/>
+    {/* <Product/> */}
+    <ProductDetails/>
     <Footer/>
     </>
   )
