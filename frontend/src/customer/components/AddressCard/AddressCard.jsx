ed6efb7b5f0c1fdd877e0a27c8ff3245c2f7873a
relan1997 <harshal.relan.201116@gmail.com>
2024-06-09 02:24:17 +0530

    Implemented the Entire check out page

--- frontend/src/customer/components/AddressCard/AddressCard.jsx
+++ frontend/src/customer/components/AddressCard/AddressCard.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const AddressCard = () => {
   return (
-    <div>AddressCard</div>
+    <div>
+    <div className='space-y-3 '>
+        <p className='font-semibold'>Raam Kapoor</p>
+        <p>Address : Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio magni aperiam facilis expedita id corrupti quo illo repellendus nobis vel porro, perferendis in fuga, blanditiis non ducimus temporibus nemo laudantium?</p>
+        <div className='space-y-1'>
+            <p className='font-semibold'>Phone Number</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quo incidunt nesciunt commodi rem aliquam in sapiente culpa, facilis iste, corporis beatae! Doloremque animi beatae explicabo repellat quo libero sunt!</p>
+        </div>
+    </div>
+    </div>
   )
 }
 
