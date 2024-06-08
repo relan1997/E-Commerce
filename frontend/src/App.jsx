6ed6881c50d5c5014ebe73b8fcf6b0697d76e7da
relan1997 <harshal.relan.201116@gmail.com>
2024-06-08 23:26:59 +0530

    Created the Cart Page without any bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import HomePage from './customer/pages/HomePage/HomePage'
 import Footer from './customer/components/Footer/Footer'
 import Product from './customer/components/Product/Product'
 import ProductDetails from './customer/components/ProductDetails/ProductDetails'
-
+import Cart from './customer/components/Cart/Cart'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -18,7 +18,8 @@ function App() {
     </div>
     {/* <HomePage/> */}
     {/* <Product/> */}
-    <ProductDetails/> {/* pls fix iska css so look from https://youtu.be/glMRU2ER1q8?t=13319 */}
+    {/* <ProductDetails/>*/} {/*pls fix iska css so look from https://youtu.be/glMRU2ER1q8?t=13319 */}
+    <Cart/>
     <Footer/>
     </>
   )
